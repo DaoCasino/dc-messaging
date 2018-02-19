bf967cae1e5f753976a39b3ce49df26b5542b512
Alex Step <alex.step.kellas@gmail.com>
2018-02-19 13:22:34 +0200

    add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    'extends': 'standard',
+
+    'globals': {},
+
+    'plugins': [],
+
+    'rules': {
+        'no-multi-spaces'              : ['off'],
+        'key-spacing'                  : ['off'],
+        'camelcase'                    : ['off'],
+        'standard/no-callback-literal' : ['off'],
+        'no-multiple-empty-lines'      : ['off'],
+        'space-in-parens'              : ['off'],
+        'no-trailing-spaces'           : ['off'],
+        'padded-blocks'                : ['off'],
+        'semi'                         : ['warn'],
+        'no-tabs'                      : ['warn'],
+        'no-irregular-whitespace'      : ['warn'],
+        'new-cap'                      : ['warn'],
+        'keyword-spacing'              : ['warn'],
+        'brace-style'                  : ['warn'],
+        'comma-spacing'                : ['warn'],
+        'spaced-comment'               : ['warn'],
+        'no-mixed-spaces-and-tabs'     : ['warn'],
+        'arrow-spacing'                : ['warn'],
+        'space-unary-ops'              : ['warn'],
+        'eol-last'                     : ['warn'],
+        'comma-dangle'                 : ['warn'],
+        'space-infix-ops'              : ['warn'],
+        'space-before-function-paren'  : ['warn'],
+        'space-before-blocks'          : ['warn'],
+        'indent'                       : ['warn']
+    }
+}
